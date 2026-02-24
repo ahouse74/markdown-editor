d06afde12bccfd2ee9425e839440f52211843328
Allison House <ajhouse05@gmail.com>
2026-02-24 22:06:23 +0100

    Milestone 4

--- app.js
+++ app.js
@@ -2,7 +2,31 @@ Vue.createApp({
     data() {
         return {
             message: '',
+            theme: 'light',
         };
+    },
+
+    mounted() {
+        this.loadFromLocalStorage();
+    },
+
+    methods: {
+        saveToStorage() {
+            localStorage.setItem('message', this.message);
+            localStorage.setItem('theme', this.theme);
+        },
+
+        loadFromLocalStorage() {
+            this.message = localStorage.getItem('message') ?? '';
+            this.theme = localStorage.getItem('theme') ?? 'light';
+            document.documentElement.setAttribute('data-bs-theme', this.theme);
+        },
+
+        toggleTheme() {
+            this.theme = this.theme === 'light' ? 'dark' : 'light';
+            document.documentElement.setAttribute('data-bs-theme', this.theme);
+            this.saveToStorage();
+        },
     },
 
     computed: {
@@ -15,6 +39,16 @@ Vue.createApp({
             return textOnly.length;
         },
 
+        themeIcon() {
+            return this.theme === 'light' ? 'dark_mode' : 'light_mode';
+        },
+
+
     },
 
+    watch: {
+        message() {
+            this.saveToStorage();
+        },
+    }
 }).mount('#app');   
